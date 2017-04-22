2590814d3aee0f271a7f160977c40dd6e660f8b5
DTT0001 <chuongluke@gmail.com>
2017-04-22 08:22:22 +0700

    Groups app better organization

--- feelings/groups/urls.py
+++ feelings/groups/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 
 from feelings.groups import  views
 urlpatterns = [
-    url(r'^create/$', views.CompanyCreate.as_view(), name='company_create')
+    url(r'^company/create/$', views.CompanyCreate.as_view(), name='company_create')
 ]
